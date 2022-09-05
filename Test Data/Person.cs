f39a20d703f7852f2c8d56e7462636cc511fdfae
Andrew Lumsden <AndrewL@forrit.com>
2022-09-05 09:38:28 +0100

    Added Address

--- Test Data/Person.cs
+++ Test Data/Person.cs
@@ -1,7 +1,17 @@
 ﻿namespace Test_Data
 {
+    public class Address
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostCode { get; set; }
+        public string StreetName { get; set; }
+    }
+
     public class Person
     {
+        public Address Address { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
 
@@ -13,8 +23,8 @@
         public Guid Id { get; set; }
         public string JobTitle { get; set; }
         public string LastName { get; set; }
-
         public string MobilePhone { get; set; }
+        public string NI { get; set; }
         public string Title { get; set; }
     }
 }
--- Test Data/Program.cs
+++ Test Data/Program.cs
@@ -1,8 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Bogus;
+using Bogus.Extensions.UnitedKingdom;
 using System.Text.Json;
 
 //Randomizer.Seed = new Random(8675309);
+
+//TODO Makes Addresses more UK based
+var randomAddress = new Faker<Test_Data.Address>("en_GB")
+    .RuleFor(x => x.City, x => x.Address.City())
+    .RuleFor(x => x.HouseNumber, x => x.Address.BuildingNumber())
+    .RuleFor(x => x.PostCode, x => x.Address.ZipCode("KY## #AW"))
+    .RuleFor(x => x.Country, x => x.Address.CountryOfUnitedKingdom())
+    .RuleFor(x => x.StreetName, x => x.Address.StreetName());
 
 var RandoPerson = new Faker<Test_Data.Person>("en_GB")
     .RuleFor(x => x.FirstName, x => x.Person.FirstName)
@@ -12,8 +21,11 @@ var RandoPerson = new Faker<Test_Data.Person>("en_GB")
     .RuleFor(x => x.Title, x => x.Name.Prefix(x.Person.Gender))
     .RuleFor(x => x.JobTitle, x => x.Name.JobTitle())
     .RuleFor(x => x.Gender, x => x.Person.Gender.ToString())
+    .RuleFor(x => x.NI, x => x.Finance.Nino())
+    .RuleFor(x => x.Address, x => randomAddress.Generate())
     .RuleFor(x => x.MobilePhone, x => x.Phone.PhoneNumber("07#########"))
     .RuleFor(x => x.Id, x => Guid.NewGuid());
 
-var users = RandoPerson.Generate(10);
+var users = RandoPerson.Generate(12);
+
 Console.WriteLine(JsonSerializer.Serialize(users, new JsonSerializerOptions() { WriteIndented = true }));
