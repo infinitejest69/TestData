c0e514a759982ebb99987eb06261cc331c4e942b
Andrew Lumsden <AndrewL@forrit.com>
2022-09-12 13:19:14 +0100

    Removed Excess projects

--- DeleteAllBranches/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using API.Helper;
-
-var helper = new APIHelper();
-var branches = await helper.GetAllbranches();
-foreach (var branch in branches.Results)
-{
-    Console.WriteLine($"Branch To Delete {branch.Name}");
-    await helper.DeleteBranchIfExists(branch.Name);
-}
